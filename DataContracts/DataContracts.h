f847d93dcc34476434f12b4b69b1357b41e52819
Sally <apple@apples-Mac-mini-3.local>
2013-02-24 15:23:22 +0200

    Initial Commit

---
+++ DataContracts/DataContracts.h
@@ -0,0 +1,13 @@
+//
+//  DataContracts.h
+//  DataContracts
+//
+//  Created by Sally on 2/24/13.
+//  Copyright (c) 2013 apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataContracts : NSObject
+
+@end
---
+++ DataContracts/DataContracts.m
@@ -0,0 +1,13 @@
+//
+//  DataContracts.m
+//  DataContracts
+//
+//  Created by Sally on 2/24/13.
+//  Copyright (c) 2013 apple. All rights reserved.
+//
+
+#import "DataContracts.h"
+
+@implementation DataContracts
+
+@end
